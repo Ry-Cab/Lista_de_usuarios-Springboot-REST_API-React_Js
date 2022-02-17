6c071b7dfdf5ec697e674b8a937c71cdf0c1e470
Ryan Cabral <cabral.reis23@gmail.com>
2022-02-17 17:36:12 -0300

    first commit

---
+++ spring/springboot-backend/src/main/java/net/javaguides/springboot/model/User.java
@@ -0,0 +1,63 @@
+package net.javaguides.springboot.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	 private Long id;
+	
+	@Column(name =  "first_name")
+	 private String firstname;
+	
+	@Column(name = "last_name")
+	 private String lastName;
+	
+	 private String email;
+	 
+	 public User() {
+		 
+	 }
+	
+	public User(String firstname, String lastName, String email) {
+		super();
+		this.firstname = firstname;
+		this.lastName = lastName;
+		this.email = email;
+	}
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	 
+	 
+}
